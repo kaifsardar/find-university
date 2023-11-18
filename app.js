30673d4f2bc07af3d35cfcd9f110c466ab93f140
Kaif.JU.CSE <90242848+kaifsardar@users.noreply.github.com>
2023-11-18 18:22:44 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
 let url="http://universities.hipolabs.com/search?country=";
 let btn=document.querySelector(".nav-search button");
+
+document.addEventListener("keypress",async (e)=>{
+    if(e.key==="Enter"){
+    let country=document.querySelector("input").value;
+    document.querySelector("input").value="";
+    colleges=await getCollege(country);
+    showResult(colleges , country);
+    }
+})
 btn.addEventListener("click",async ()=>{
     let country=document.querySelector("input").value;
     document.querySelector("input").value="";
@@ -18,7 +27,7 @@ async function getCollege(country){
 }
 
 showResult=(colleges ,country)=>{
-    let title=`Search result for ${country}:- <span>total ${colleges.data.length} result found</span>`
+    let title=`Search result for ${country}:- <span>total ${colleges.data.length} results found</span>`
     document.querySelector(".result-title").innerHTML=title;
     document.querySelector("ol").innerHTML="";
     for (college of colleges.data){
